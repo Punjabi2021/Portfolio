af5c2113c3be2b86547c6600255e5830b4a70ea1
Bhawandeep Singh <pavan@Bhawandeeps-MacBook-Air.local>
2022-04-22 15:27:44 -0700

    added contact me

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import Header from "./components/Header";
 import Welcome from "./components/Welcome";
 import Footer from "./components/Footer";
+import Contact from "./components/Contact";
 function App() {
   return (
     <div className="App">
       <Header />
       <Welcome />
       <Footer />
+      <Contact />
     </div>
   );
 }
