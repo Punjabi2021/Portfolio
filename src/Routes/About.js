eae962c9ff6ad045ef9d5ecf74fa71a8a840951d
Bhawandeep Singh <pavan@Bhawandeeps-MacBook-Air.local>
2022-04-26 11:28:16 -0700

    working on the About me

--- src/Routes/About.js
+++ src/Routes/About.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import Header from '../components/Header'
 import Footer from "../components/Footer";
+import './About.css'
+import PavanPhoto from "../assets/PavanPhoto"
 export default function About() {
   return (
     <div>
         <Header />
-        <h1>About me</h1>
+<div>
+<img src={PavanPhoto} />
+</div>
+        <h1>I'm Pavan Singh an U.S.M.C. Veteran who also is a web developer </h1>
+        <p>with a vast array of knowledge in many different front end and back end languages, responsive frameworks, databases, and best code practices. My objective is simply to be the best web developer that I can be and to contribute to the technology industry all that I know and can do.</p>
         <Footer />
     </div>
   )
